d1e1dc951c9acbfe9703fb22628a35c0d37ab970
Michael Ciabuca <ciabuca@gmail.com>
2020-07-06 01:17:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  counter = 0
+  while counter < n 
+    puts "#{n} SECOND(S)!"
+    counter += 1 
+  end
+  puts "HAPPY NEW YEAR!"
   
 end
