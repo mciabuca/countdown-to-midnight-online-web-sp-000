9a39775201562353aa254dbd4077b23fe3a40c6a
Michael Ciabuca <ciabuca@gmail.com>
2020-07-06 01:35:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,13 @@ def countdown(number)
   puts "HAPPY NEW YEAR!"
 end
 
-countdown(10)
+def countdown_with_sleep(number)
+  while number > 0 
+    sleep()
+    puts "#{number} SECOND(S)!"
+     number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(10)
